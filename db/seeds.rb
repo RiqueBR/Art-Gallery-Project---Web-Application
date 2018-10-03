b5289197e3845452b2a255522b96ea6be4fb9ff0
Henrique Pereira Batista <h.p.batista20@gmail.com>
2018-10-03 12:01:25 +0100

    Project finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@ Artist.delete_all()
 artist1 = Artist.new({
   "first_name" => "Fiona",
   "last_name" => "Arnott",
-  "artist_info" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  "artist_img" => "/images/dog9.jpg"
+  "artist_info" => "Born in Lennoxtown, Fiona aims to change the concept of artistic collage by using digital methods",
+  "artist_img" => "/images/artist3.jpg"
   })
 
   artist1.save()
@@ -17,8 +17,8 @@ artist1 = Artist.new({
   artist2 = Artist.new({
     "first_name" => "Henrique",
     "last_name" => "Batista",
-    "artist_info" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "artist_img" => "/images/dog10.jpg"
+    "artist_info" => "Origibally from Brazil, Henrique is Inspired by street photography and his surroundings",
+    "artist_img" => "/images/artist1.jpg"
     })
 
     artist2.save()
@@ -26,11 +26,11 @@ artist1 = Artist.new({
 
 
     exhibition1 = Exhibition.new({
-      "name" => "Drawings of My Glasgow",
+      "name" => "Digital Collage",
       "category" => "Artistic Drawing",
       "artist_id" => artist1.id,
-      "exhibition_info" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "exhibit_img" => "/images/dog1.jpg"
+      "exhibition_info" => "A collection of thoughtful and provoking images that challenges concepts.",
+      "exhibit_img" => "/images/digital-collage.jpg"
       })
 
       exhibition1.save()
@@ -39,8 +39,8 @@ artist1 = Artist.new({
         "name" => "Idas e Vindas Fotográficas",
         "category" => "Photography",
         "artist_id" => artist2.id,
-        "exhibition_info" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "exhibit_img" => "/images/dog5.jpg"
+        "exhibition_info" => "Images of what I call home.",
+        "exhibit_img" => "/images/rio-photo.jpg"
         })
 
         exhibition2.save()
